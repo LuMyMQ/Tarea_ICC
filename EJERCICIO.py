5e892248ea49a68db16a05206c7615d767d7e594
Luciana <luciana.melgarejo@utec.edu.pe>
2024-09-21 10:39:55 -0500

    Primer commit

---
+++ EJERCICIO.py
@@ -0,0 +1,8 @@
+#Calcular el área y el volumen de un paralelepípedo:
+a=10
+b=25
+c=50
+area= 2*((a*b)+(b*c)+(c*a))
+volumen=a*b*c
+print("El área de un paralelepípedo es:",area)
+print("El volumen de un paralelepípedo es:",volumen)
